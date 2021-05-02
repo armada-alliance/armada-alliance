d7ee9182f16eaae582dc23372459c58a69d8e292
Olivier Andriessen <olivier@andev.nl>
2021-05-02 13:19:30 +0000

    feat: 🎸 read git file creation date instead

--- scripts/pools.js
+++ scripts/pools.js
@@ -1,17 +1,21 @@
+const { execSync } = require("child_process")
 const fs = require("fs")
 const basePath = __dirname + "/.."
 
 const files = fs.readdirSync(basePath + "/registry")
 
+function getGitCreationTimeForFile(file) {
+    const dateString = execSync(`git log --format=%aD ${file} | tail -1`).toString().split("\n").join("")
+    return new Date(dateString)
+}
+
 const pools = files
     .map(file => {
         const data = require(basePath + "/registry/" + file)
-        const stats = fs.statSync(basePath + "/registry/" + file)
-        console.log(stats)
-        const { birthtime } = stats
+        const memberSince = getGitCreationTimeForFile(basePath + "/registry/" + file)
         return {
             ...data,
-            memberSince: birthtime
+            memberSince
         }
     })
     .sort((a, b) =>
