9aab2611a4e86a63b0b4c644c3710e57b24a6795
Olivier Andriessen <olivier@andev.nl>
2021-06-13 20:30:08 +0000

    feat: 🎸 save changes

--- services/website/pages/index.js
+++ services/website/pages/index.js
@@ -1,6 +1,6 @@
-import pages from '../src/pages.json'
 import DynamicPage from '../src/app/DynamicPage'
 import fs from 'fs/promises'
+import path from 'path'
 
 export default function IndexRoute(props) {
     return <DynamicPage {...props} />
@@ -8,7 +8,9 @@ export default function IndexRoute(props) {
 
 export async function getStaticProps() {
 
-    const data = await fs.readFile('/app/src/page-data/index.json')
+    const appPath = path.resolve(process.cwd())
+
+    const data = await fs.readFile(path.join(appPath, 'src/page-data/index.json'))
 
     const page = JSON.parse(data)
 
