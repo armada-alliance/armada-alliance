bc8f9f886b44de31b78180b6342b0a22522e612f
sublayerio <44947294+sublayerio@users.noreply.github.com>
2021-06-13 20:30:08 +0000

    feat: 🎸 save changes

--- services/website/pages/[...slug].js
+++ services/website/pages/[...slug].js
@@ -2,6 +2,7 @@ import compact from 'lodash/compact'
 import pages from '../src/pages.json'
 import DynamicPage from '../src/app/DynamicPage'
 import fs from 'fs/promises'
+import path from 'path'
 
 export default function SlugRoute(props) {
   return <DynamicPage {...props} />
@@ -10,7 +11,8 @@ export default function SlugRoute(props) {
 export async function getStaticProps(ctx) {
 
   const slug = '/' + ctx.params.slug.join('/')
-  const data = await fs.readFile(`/app/src/page-data/${slug}.json`)
+  const appPath = path.resolve(process.cwd())
+  const data = await fs.readFile(path.join(appPath, `src/page-data/${slug}.json`))
 
   const page = JSON.parse(data)
 
