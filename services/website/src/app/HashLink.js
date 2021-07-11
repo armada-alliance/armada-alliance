9c11a7dd969f65986ceac477cc748f67b018f717
sublayerio <44947294+sublayerio@users.noreply.github.com>
2021-07-11 11:37:33 +0000

    feat: 🎸 add fixes

--- services/website/src/app/HashLink.js
+++ services/website/src/app/HashLink.js
@@ -1,3 +1,4 @@
+import { ExternalLinkIcon } from '@heroicons/react/outline'
 import CopyToClipboard from './CopyToClipboard'
 import Tooltip from './Tooltip'
 
@@ -10,8 +11,11 @@ export default function HashLink({ href, hash }) {
     return (
         <div className="flex items-center">
             <Tooltip text="Go to CardanoScan explorer">
-                <a href={href} target="_blank" className="inline-flex bg-gray-50 text-gray-500 rounded-lg px-4 py-3 tracking-wider text-xs sm:text-sm hover:underline cursor-pointer">
-                    {formatAddress(hash)}
+                <a href={href} target="_blank" className="inline-flex items-center space-x-2 bg-gray-50 text-gray-500 rounded-lg px-4 py-3 tracking-wider text-xs sm:text-sm hover:underline cursor-pointer">
+                    <div>
+                        {formatAddress(hash)}
+                    </div>
+                    <ExternalLinkIcon className="h-5" />
                 </a>
             </Tooltip>
             <CopyToClipboard text={href} />
