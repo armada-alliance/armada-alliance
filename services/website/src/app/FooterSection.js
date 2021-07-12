d7d0211462448c8fa658316d988a49771f647ab2
sublayerio <44947294+sublayerio@users.noreply.github.com>
2021-07-12 11:04:09 +0000

    feat: 🎸 remove footer

--- services/website/src/app/FooterSection.js
+++ services/website/src/app/FooterSection.js
@@ -68,76 +68,8 @@ export default function Footer() {
 
     return (
         <footer className="bg-white" aria-labelledby="footerHeading">
-            <h2 id="footerHeading" className="sr-only">
-                Footer
-        </h2>
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-                <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-                    <div className="grid grid-cols-2 gap-8 xl:col-span-2">
-                        <div className="md:grid md:grid-cols-2 md:gap-8">
-                            <div>
-                                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Solutions</h3>
-                                <ul className="mt-4 space-y-4">
-                                    {/* {navigation.solutions.map((item) => (
-                                        <li key={item.name}>
-                                            <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))} */}
-                                </ul>
-                            </div>
-                            <div className="mt-12 md:mt-0">
-                                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Support</h3>
-                                <ul className="mt-4 space-y-4">
-                                    {/* {navigation.support.map((item) => (
-                                        <li key={item.name}>
-                                            <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))} */}
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="md:grid md:grid-cols-2 md:gap-8">
-                            <div>
-                                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Company</h3>
-                                <ul className="mt-4 space-y-4">
-                                    {/* {navigation.company.map((item) => (
-                                        <li key={item.name}>
-                                            <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))} */}
-                                </ul>
-                            </div>
-                            <div className="mt-12 md:mt-0">
-                                <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">Legal</h3>
-                                <ul className="mt-4 space-y-4">
-                                    {/* {navigation.legal.map((item) => (
-                                        <li key={item.name}>
-                                            <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
-                                                {item.name}
-                                            </a>
-                                        </li>
-                                    ))} */}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mt-8 xl:mt-0">
-                        <h3 className="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                            Subscribe to our newsletter
-              </h3>
-                        <p className="mt-4 text-base text-gray-500">
-                            The latest news, articles, and resources, sent to your inbox weekly.
-              </p>
-                        <SubscribeForm />
-                    </div>
-                </div>
-                <div className="mt-8 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between">
+                <div className="md:flex md:items-center md:justify-between">
                     <div className="flex space-x-6 md:order-2">
                         {/* {navigation.social.map((item) => (
                             <a key={item.name} href={item.href} target="_blank" className="text-gray-400 hover:text-gray-500">
