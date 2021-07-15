2784771bee352f484aa33ac4028415e32c4ac062
Olivier Andriessen <olivier@andev.nl>
2021-07-15 19:12:20 +0000

    feat: 🎸 fix

--- services/website/src/app/StatsWidgetPage.js
+++ services/website/src/app/StatsWidgetPage.js
@@ -1,15 +1,22 @@
+import { useEffect, useState } from 'react'
 import Component from './Component'
 import StatsSection from './StatsSection'
 
 export default function StatsWidgetPage({ components }) {
 
-    let searchParams = process.browser ? new URLSearchParams(location.search) : null
+    const [style, setStyle] = useState(null)
 
-    const image = searchParams && searchParams.get('context') === 'image'
+    useEffect(() => {
+        let searchParams = process.browser ? new URLSearchParams(window.location.search) : null
+        const image = searchParams && searchParams.get('context') === 'image'
+        if (image) {
+            setStyle({ fontFamily: 'Noto Sans' })
+        }
+    })
 
     return (
-        <div style={image ? { fontFamily: 'Noto Sans' } : null}>
-            <Component use={StatsSection} data={components.StatsSection} image={image} />
+        <div style={style}>
+            <Component use={StatsSection} data={components.StatsSection} />
         </div>
     )
 }
