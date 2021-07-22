6b48350383c297c2c0170463b1b0ec4305c93742
Olivier Andriessen <olivier@andev.nl>
2021-07-22 20:07:02 +0000

    feat: 🎸 fix badges outlining

--- services/website/src/app/PageSocials.js
+++ services/website/src/app/PageSocials.js
@@ -3,27 +3,33 @@ import * as icons from './icons'
 function Socials({ socials, badges }) {
 
     return (
-        <div className="mx-auto inline-flex items-center space-x-2 text-sm sm:text-base">
-            {(socials || []).map((social) => {
+        <div className="mx-auto text-sm sm:text-base">
+            <div className="-m-2 flex flex-wrap items-center justify-center">
+                {(socials || []).map((social) => {
 
-                const Icon = icons[social.icon]
+                    const Icon = icons[social.icon]
 
-                return (
-                    <a key={social.id} href={social.link.href} title={social.link.title} target="_blank" className="flex flex-nowrap items-center px-3 h-8 justify-center space-x-2 rounded-lg text-white hover:opacity-50 transition-opacity duration-50" style={{ background: social.color }}>
-                        <Icon className="h-5" />
-                    </a>
-                )
+                    return (
+                        <div key={social.id} className="p-1">
+                            <a href={social.link.href} title={social.link.title} target="_blank" className="flex flex-nowrap items-center px-3 h-8 justify-center space-x-2 rounded-lg text-white hover:opacity-50 transition-opacity duration-50" style={{ background: social.color }}>
+                                <Icon className="h-5" />
+                            </a>
+                        </div>
+                    )
 
-            })}
-            {(badges || []).map((badge) => {
+                })}
+                {(badges || []).map((badge) => {
 
-                return (
-                    <div key={badge.name} className="flex flex-nowrap items-center px-3 h-8 justify-center text-sm space-x-2 rounded-lg text-white" style={{ background: badge.color }}>
-                        {badge.name}
-                    </div>
-                )
+                    return (
+                        <div key={badge.name} className="p-1">
+                            <div className="flex flex-nowrap whitespace-nowrap items-center px-3 h-8 justify-center text-sm space-x-2 rounded-lg text-white" style={{ background: badge.color }}>
+                                {badge.name}
+                            </div>
+                        </div>
+                    )
 
-            })}
+                })}
+            </div>
         </div>
     )
 }
