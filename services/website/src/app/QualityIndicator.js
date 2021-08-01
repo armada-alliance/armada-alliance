42a6c475a1e937e1f0b2954bff20cf0418ae9924
sublayerio <44947294+sublayerio@users.noreply.github.com>
2021-08-01 13:19:21 +0000

    style: 💄 cosmetics

--- services/website/src/app/QualityIndicator.js
+++ services/website/src/app/QualityIndicator.js
@@ -8,9 +8,9 @@ export default function QualityIndicator({ qualityScore }) {
             className={cx(
                 "inline-flex items-center px-2 py-1 rounded-full text-xs space-x-1 font-medium",
                 {
-                    "bg-green-100 text-green-500 dark:bg-green-500 dark:text-white": qualityScore >= 75,
-                    "bg-yellow-100 text-yellow-500 dark:bg-yellow-500 dark:text-white": qualityScore < 75 && qualityScore >= 50,
-                    "bg-red-100 text-red-500 dark:bg-red-500 dark:text-white": qualityScore < 50,
+                    "bg-green-100 text-green-500": qualityScore >= 75,
+                    "bg-yellow-100 text-yellow-500": qualityScore < 75 && qualityScore >= 50,
+                    "bg-red-100 text-red-500": qualityScore < 50,
                 }
             )}
         >
