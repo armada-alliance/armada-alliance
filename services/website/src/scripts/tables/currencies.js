6a99f01d9f1679094e7f53ec7813cae475525b04
sublayerio <info@sublayer.io>
2022-01-27 02:38:13 +0100

    feat: 🎸 temporarily hardcode currency data

--- services/website/src/scripts/tables/currencies.js
+++ services/website/src/scripts/tables/currencies.js
@@ -27,15 +27,17 @@ module.exports = {
 
         console.log(`https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=${currency_names}`)
 
-        const currency_data = await axios
-            .request({
-                method: 'get',
-                url: `https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=${currency_names}`,
-                headers: {
-                    'content-type': 'application/json'
-                }
-            })
-            .then((res) => res.data.cardano)
+        // const currency_data = await axios
+        //     .request({
+        //         method: 'get',
+        //         url: `https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=${currency_names}`,
+        //         headers: {
+        //             'content-type': 'application/json'
+        //         }
+        //     })
+        //     .then((res) => res.data.cardano)
+
+        const currency_data = { "eur": 0.961595, "usd": 1.08, "btc": 2.919e-05, "aud": 1.52, "cny": 6.83, "myr": 4.53, "jpy": 124.02, "thb": 35.83 }
 
         return currencies.map(currency => {
 
