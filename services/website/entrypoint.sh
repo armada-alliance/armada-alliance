503750c1b8b85792f4fa73dbc10e593c52d99fe0
Wael <wael.t.ivie@gmail.com>
2025-02-19 12:43:09 -0800

    push changes

--- services/website/entrypoint.sh
+++ services/website/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+# 1- Install npm packages
+npm install react-popper --save
 npm install
-
-# npm run dev
+# # 2 - Load environmental variables
+# set -a && source .env && set +a
+# 3 - Execute pre-build tasks to generate static data 
+node src/scripts/tasks/all.js
+# 4 - Launch Next dev (pass openssl-legacy-provider as NODE_OPTIONS if Node version >= 18) 
+NODE_OPTIONS=--openssl-legacy-provider npm run dev
 
 echo "Press [CTRL+C] to stop.."
 while true
